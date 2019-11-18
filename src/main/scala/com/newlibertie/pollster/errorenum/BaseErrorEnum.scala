040d671efb485ce3bde0986c70678eb1f0db7626
harmindar <neat.vision@gmail.com>
2019-11-18 10:56:39 -0700

    Added comments, fixed a typo and made registry private

--- src/main/scala/com/newlibertie/pollster/errorenum/BaseErrorEnum.scala
+++ src/main/scala/com/newlibertie/pollster/errorenum/BaseErrorEnum.scala
@@ -2,6 +2,14 @@ package com.newlibertie.pollster.errorenum
 
 import scala.collection.mutable
 
+/***
+  * A class at the root of all error enum derivations
+  *   responsible for validating uniqueness of the name of the errEnumName and non-overlapping id ranges
+  *   registers the EnumClass
+  * @param errEnumName  The unique name of the derived class
+  * @param initial  the id of the first enum variable in the class with name $errEnumName
+  * @param capacity the number of unique ids for the enum variables in the class with name $errEnumName
+  */
 class BaseErrorEnum(val errEnumName: String, val initial:Int, val capacity:Int) extends Enumeration{
   require(EnumStore.register(errEnumName, initial, capacity), "Failed to register: "+errEnumName)
   type AppErr = AEVal
@@ -31,18 +39,21 @@ class BaseErrorEnum(val errEnumName: String, val initial:Int, val capacity:Int) 
   }
 }
 
+/**
+  * Helping Object  providing validations and register each EnumClass derived from BaseErrorEnum
+  */
 private object EnumStore {
   class ErrEnumRange(val initial: Int, val capacity: Int)
-  val registery: mutable.Map[String, ErrEnumRange] = new mutable.HashMap
+  private val registry: mutable.Map[String, ErrEnumRange] = new mutable.HashMap
   def register(errEnumName: String, initial:Int, capacity:Int):Boolean = {
-    if (registery.contains(errEnumName) ||
-      (registery.valuesIterator.exists(er => er.initial < (initial + capacity) &&
+    if (registry.contains(errEnumName) ||
+      (registry.valuesIterator.exists(er => er.initial < (initial + capacity) &&
         initial < (er.initial + er.capacity)))){
       false
     }
     else
     {
-      registery(errEnumName) = new ErrEnumRange(initial, capacity)
+      registry(errEnumName) = new ErrEnumRange(initial, capacity)
       true
     }
   }
