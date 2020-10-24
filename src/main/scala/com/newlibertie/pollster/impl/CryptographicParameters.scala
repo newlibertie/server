d6f77e50d26d70d7b49a70c84a3b5bff512c7565
vivekpathak <vpathak+github@orgmeta.com>
2020-10-24 14:01:43 -0400

    moved to within class to match meaning of self generated vs sent params

--- src/main/scala/com/newlibertie/pollster/impl/CryptographicParameters.scala
+++ src/main/scala/com/newlibertie/pollster/impl/CryptographicParameters.scala
@@ -20,9 +20,9 @@ case class CryptographicParameters
   large_prime_p:BigInteger = CryptographicParameters.probablePrime(),
   generator_g:BigInteger = CryptographicParameters.random(),
   private_key_s:BigInteger = CryptographicParameters.random(),
-  zkp_generator_G:BigInteger = CryptographicParameters.random(), // TODO: to be saved, processed, and carried end to end
 )
 {
+  val zkp_generator_G:BigInteger = CryptographicParameters.random().mod(large_prime_p)
   val public_key_h = generator_g.modPow(private_key_s, large_prime_p)
 }
 
