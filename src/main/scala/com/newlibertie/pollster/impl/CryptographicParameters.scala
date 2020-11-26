49963fcda8949d74fcf8470bbca2bc22aa8de953
vivekpathak <vpathak+github@orgmeta.com>
2020-11-26 16:11:39 -0500

    assertions expected to avoid indetermistic choices on ballot casting

--- src/main/scala/com/newlibertie/pollster/impl/CryptographicParameters.scala
+++ src/main/scala/com/newlibertie/pollster/impl/CryptographicParameters.scala
@@ -4,6 +4,7 @@ import java.math.BigInteger
 
 object CryptographicParameters {
   val BITS = 32 // 1000
+  val TWO_POW_BITS_SQUARE = new BigInteger("2").pow(2*BITS)
   import java.security.SecureRandom;
   private val rng = new SecureRandom()
 
@@ -25,10 +26,25 @@ case class CryptographicParameters
   val zkp_generator_G:BigInteger = CryptographicParameters.random().mod(large_prime_p)
   val public_key_h = generator_g.modPow(private_key_s, large_prime_p)
 
+  // execution should truly never throw here because the probabilistic prime should be coprime to
+  // a generator smaller than itself
+  if(!this)
+    throw new java.lang.Error("generator not defined");
+
   def getPublicKeyValueString: String = {
     s"""
       |"public_key_h":"${public_key_h}"
       |""".stripMargin
+  }
+
+  def unary_!(): Boolean = {
+    if(!large_prime_p.gcd(generator_g).equals(BigInteger.ONE) ||
+       !large_prime_p.gcd(zkp_generator_G).equals(BigInteger.ONE)
+    ) {
+      true
+    } else {
+      false
+    }
   }
 }
 
